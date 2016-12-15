6f65526301ac912007bdae937438608d0140491a
AZKRON\Hugo <hugobeny@gmail.com>
2016-12-15 12:25:35 +0100

    Database and htaccess rename

---
+++ src/anc3_gr1/ANC3_GR1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package anc3_gr1;
+
+/**
+ *
+ * @author Hugo
+ */
+public class ANC3_GR1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
