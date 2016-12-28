a52c3983ecb15e3f593d773ab5d379bc42b6aad5
AZKRON\Hugo <hugobeny@gmail.com>
2016-12-28 17:53:54 +0100

    Labyrinth class done

---
+++ src/Model/Mushroom.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import Control.Type;
+
+/**
+ *
+ * @author Hugo
+ */
+public class Mushroom implements Consumable{
+
+    @Override
+    public void Consume() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getX() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getY() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Type getType() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
