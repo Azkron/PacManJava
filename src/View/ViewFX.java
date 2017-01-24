61ebb66d00fbeedff7a3022a782ba06a92e8953e
tidiane <tidiane@DESKTOP-3RHCTQS.home>
2017-01-24 16:13:05 +0100

    commit with all images + info on the state of the game

--- src/View/ViewFX.java
+++ src/View/ViewFX.java
@@ -11,11 +11,18 @@ import Model.GameState;
 import java.util.Observable;
 import java.util.Observer;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -38,9 +45,16 @@ public class ViewFX implements Observer{
     private final Image imgWall = new Image("file:images/wall.png");
     private final Image imgWhite = new Image("file:images/white.gif");
     
+    private final TextField tfScore = new TextField();
+    private final TextField tfLives = new TextField();
+    private final TextField tfPhantoms = new TextField();
+    private final TextField tfPacGums = new TextField();
+    
+    private HBox boxInfo;
+    int ySize, xSize, height, width;
+    
     private static final int SIZE = 20;
-    private Canvas canvas = new Canvas();
-    private GraphicsContext gc = canvas.getGraphicsContext2D();
+    private GraphicsContext gc;
     
     private static ViewFX instance; 
     
@@ -57,27 +71,55 @@ public class ViewFX implements Observer{
     }
     
     
-    public void initialize(Stage primaryStage){   
+    public void initialize(Stage primaryStage){
+        
         Type[][] lab = GameState.getInstance().getLabView();
-        int ySize = lab.length, xSize = lab[0].length, height = ySize * SIZE, width = xSize * SIZE;
+        ySize = lab.length;
+        xSize = lab[0].length;
+        height = ySize * SIZE;
+        width = xSize * SIZE;
         
-        Pane root = new Pane() {
-            @Override
-            protected void layoutChildren() {
-                canvas.setWidth(width);
-                canvas.setHeight(height);
-            }
-        };
         
-        canvas = new Canvas();
+        BorderPane root = new BorderPane(); //{
+//            @Override
+//            protected void layoutChildren() {
+//                canvas.setWidth(width);
+//                canvas.setHeight(height);
+//                
+//            }
+//        };
+        
+        Canvas canvas = new Canvas(width, height);
+        boxInfo = new HBox();
+        layout();
         gc = canvas.getGraphicsContext2D();
-        root.getChildren().add(canvas);
+        //boxInfo.setPrefHeight(50);
+        root.setTop(canvas);
+
+        root.setBottom(boxInfo);
         
         Scene scene = new Scene(root, width, height);
         primaryStage.setTitle("PAC MAN FX GR1");
         primaryStage.setScene(scene);
         primaryStage.show();
         
+    }
+    
+    private void layout() {
+        //beautify();
+        boxInfo.getChildren().addAll(tfScore, tfLives, tfPhantoms, tfPacGums);
+    }
+    
+    private void beautify() {
+        tfScore.setPrefColumnCount(3);
+        tfLives.setPrefColumnCount(3);
+        tfPhantoms.setPrefColumnCount(3);
+        tfPacGums.setPrefColumnCount(3);
+        
+        boxInfo.setSpacing(10);
+        boxInfo.setPadding(new Insets(5, 5, 0, 5));
+        boxInfo.setAlignment(Pos.BOTTOM_CENTER);
+
     }
     
     @Override
@@ -88,7 +130,7 @@ public class ViewFX implements Observer{
     private void drawLabyrinth(GameState g) {
         
         gc.setFill(Color.WHITE);
-        gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        gc.fillRect(0, 0, width, height);
         
         Type[][] lab = g.getLabView();
         for(int y=0; y < lab.length; y++ ) 
@@ -124,16 +166,10 @@ public class ViewFX implements Observer{
     }
     
     public void displayInfo() {
-        System.out.println("");
-        System.out.println("-------------------------------------------------------");
-        System.out.print("Lives: " +GameState.getLives());
-        System.out.print("   Score: " +GameState.getScore());
-        System.out.print("   Phantoms: " +GameState.getPhantoms());
-        System.out.print("   Pac-Gums: " +GameState.getPacGum());
-        System.out.println("");
-        System.out.println("-------------------------------------------------------");
-        
+        tfLives.setText("   Lives: " +GameState.getLives());
+        tfScore.setText("   Score: " +GameState.getScore());
+        tfPhantoms.setText("   Phantoms: " +GameState.getPhantoms());
+        tfPacGums.setText("   Pac-Gums: " +GameState.getPacGum());
     }
-
-    
-}
+      
+ }
