f5aea06b3b75c634ccf56216658331c4e2d09c80
AZKRON\Hugo <hugobeny@gmail.com>
2017-04-07 15:20:10 +0200

    phantom small change

--- src/Model/Phantom.java
+++ src/Model/Phantom.java
@@ -173,5 +173,10 @@ public class Phantom extends Character{
     GameObject deepCopy(GameObject g) {
         
     }
+
+    @Override
+    void activate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
